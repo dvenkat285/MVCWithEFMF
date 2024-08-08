2d210d9ca80187059e7205cdded421997ce05ec3
dvenkat285 <116060501+dvenkat285@users.noreply.github.com>
2024-08-08 19:16:25 +0530

    Add project files.

---
+++ MVCWithEFMF/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCWithEFMF.MvcApplication" Language="C#" %>
---
+++ MVCWithEFMF/Models/TestEF.edmx.sql
@@ -0,0 +1,86 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 08/05/2024 09:07:53
+-- Generated from EDMX file: D:\MVC\MVCWithEFMF\MVCWithEFMF\Models\TestEF.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [MyDatabase];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'Departments'
+CREATE TABLE [dbo].[Departments] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Dname] varchar(50)  NOT NULL,
+    [Location] varchar(50)  NOT NULL
+);
+GO
+
+-- Creating table 'Employees'
+CREATE TABLE [dbo].[Employees] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Ename] varchar(50)  NOT NULL,
+    [Job] varchar(50)  NOT NULL,
+    [Salary] decimal(18,2)  NOT NULL,
+    [Status] bit  NOT NULL,
+    [DepartmentId] int  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [Id] in table 'Departments'
+ALTER TABLE [dbo].[Departments]
+ADD CONSTRAINT [PK_Departments]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'Employees'
+ALTER TABLE [dbo].[Employees]
+ADD CONSTRAINT [PK_Employees]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- Creating foreign key on [DepartmentId] in table 'Employees'
+ALTER TABLE [dbo].[Employees]
+ADD CONSTRAINT [FK_DepartmentEmployee]
+    FOREIGN KEY ([DepartmentId])
+    REFERENCES [dbo].[Departments]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_DepartmentEmployee'
+CREATE INDEX [IX_FK_DepartmentEmployee]
+ON [dbo].[Employees]
+    ([DepartmentId]);
+GO
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
